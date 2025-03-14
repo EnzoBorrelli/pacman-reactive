17806843d06be9dff3137bdbc2f95b40667995f5
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-14 16:12:12 -0300

    tiles enum

---
+++ client/app/components/enums/tiles.ts
@@ -0,0 +1,11 @@
+export enum Tiles {
+    cornerWall = "cornerWall",
+    doubleWall = "doubleWall",
+    simpleWall = "singleWall",
+    doubleCorner = "doubleCorner",
+    simpleCorner = "singleCorner",
+    longCorner = "longCorner",
+    miniCorner = "miniCorner",
+    rectangle = "rectangle",
+    squareCorner = "squareCorner",
+}
