e6c3470b034e3c5e14acb84d83e1b34a66f8cfa1
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-14 17:00:06 -0300

    move code to mainMenu scene

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
-import Logo from "~/components/ui/logo";
+import { useSelector } from "react-redux";
+import { Scenes } from "~/components/enums/scene";
+import SceneDisplay from "~/components/sceneDisplay";
+import ClickBtn from "~/components/utils/clickBtn";
+import { RootState } from "~/store";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,64 +13,21 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const scene = useSelector((state: RootState) => state.scene);
   return (
-    <main className="flex flex-col items-center w-full p-4 h-dvh bg-slate-950">
-      <Logo/>
-      <ul className="flex flex-col items-center gap-4 mt-10 text-lg font-bold tracking-wider text-center text-orange-300">
-        <li className="flex items-center justify-center gap-4 group">
-          <div className="hidden group-hover:block w-3 h-3 -rotate-[135deg] border-b-4 border-l-4 border-transparent border-b-slate-50 border-l-slate-50"></div>
-          <button
-            //onMouseEnter={() => SoundPlayer("ui", "hover")}
-            //onClick={() => useBtn("play")}
-            className=" group-hover:text-yellow-300"
-          >
-            Play
-          </button>
-        </li>
-        <li className="flex items-center justify-center gap-4 group">
-          <div className="hidden group-hover:block w-3 h-3 -rotate-[135deg] border-b-4 border-l-4 border-transparent border-b-slate-50 border-l-slate-50"></div>
-          <button
-            //onMouseEnter={() => SoundPlayer("ui", "hover")}
-            //onClick={() => useBtn("sprites")}
-            className=" group-hover:text-yellow-300"
-          >
-            Extras
-          </button>
-        </li>
-        <li className="flex items-center justify-center gap-4 group">
-          <div className="hidden group-hover:block w-3 h-3 -rotate-[135deg] border-b-4 border-l-4 border-transparent border-b-slate-50 border-l-slate-50"></div>
-          <button
-            //onMouseEnter={() => SoundPlayer("ui", "hover")}
-           // onClick={() => useBtn("")}
-            className=" group-hover:text-yellow-300"
-          >
-            About
-          </button>
-        </li>
-      </ul>
-
-      <footer className="flex flex-col items-center gap-4 mt-20 font-bold tracking-wider text-center text-md text-slate-300">
-        <a
-          className="text-red-600 hover:text-yellow-600"
-          href="https://www.namco.co.jp/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          NAMCOT
-        </a>
-        <p>
-          © 1980 1990 NAMCO LTD | ALL RIGHTS RESERVED | CODED BY{" "}
-          <a
-            className="text-teal-600 hover:text-yellow-600"
-            href="https://enzo-borrelli-web.vercel.app/
-            "
-            target="_blank"
-            rel="noreferrer"
-          >
-            ENDY KAISHI®
-          </a>
-        </p>
-      </footer>
+    <main className="flex flex-col items-center w-full p-4 bg-slate-950 h-dvh">
+      <h3 className="mb-4 font-bold tracking-wider text-center text-md text-slate-300">
+        TOP SCORE: 0
+      </h3>
+      <button
+        onClick={() => ClickBtn({scene: Scenes.mapSelector, soundData: {folder: "ui", audio: "menu"}})}
+        className={`${
+          scene.scene === Scenes.mainMenu ? "hidden" : "flex"
+        } text-slate-300 bg-slate-800 ring-2 ring-slate-500 px-2 py-1 rounded-md`}
+      >
+        Main Menu
+      </button>
+      <SceneDisplay />
     </main>
   );
 }
