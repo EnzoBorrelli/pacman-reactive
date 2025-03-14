e886519ed4534360203876f0a71d6e8d92c42fa0
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-14 16:12:24 -0300

    global enums

---
+++ client/app/components/enums/global.ts
@@ -0,0 +1,18 @@
+export enum Direction {
+  up = "up",
+  down = "down",
+  left = "left",
+  right = "right",
+}
+
+export enum CharacterSize {
+  challenge = 16,
+  classic = 32,
+  small = 48,
+}
+
+export enum objectSize {
+    challenge = 8,
+    classic = 16,
+    small = 24,
+}
