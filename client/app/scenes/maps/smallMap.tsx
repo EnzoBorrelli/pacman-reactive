638a052353c3a45fa8b48063777938c5aad64f92
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-18 15:19:13 -0300

    Update smallMap.tsx

--- client/app/scenes/maps/smallMap.tsx
+++ client/app/scenes/maps/smallMap.tsx
@@ -2,14 +2,12 @@ import GameController from "~/components/controllers/gameController";
 import PacmanDebug from "~/components/utils/debug/pacmanDebug";
 
 export default function SmallMap() {
-  
-
   return (
     <main className="flex flex-col items-center w-full p-4 pt-6 h-dvh">
       <div className="flex flex-row">
-        <PacmanDebug isDebug={false}/>
+        <PacmanDebug isDebug={false} />
+        <GameController />
       </div>
-      <GameController/>
     </main>
   );
 }
