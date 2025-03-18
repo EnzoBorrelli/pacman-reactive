d4f0d6dccd29ceb2575473145e3d418523225aff
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-18 11:38:10 -0300

    Update soundPlayer.ts

--- client/app/components/utils/soundPlayer.ts
+++ client/app/components/utils/soundPlayer.ts
@@ -6,5 +6,4 @@ interface iSoundPlayer {
 export default function SoundPlayer({ folder, audio }: iSoundPlayer) {
   const sound = `/assets/sounds/${folder}/${audio}.wav`;
   new Audio(sound).play();
-  console.log("Playing sound:", sound);
 }
