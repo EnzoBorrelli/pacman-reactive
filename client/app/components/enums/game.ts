0c3cde1fdf5a548baa517dc92a396824d2d75774
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-18 15:19:38 -0300

    added score colors

--- client/app/components/enums/game.ts
+++ client/app/components/enums/game.ts
@@ -4,3 +4,12 @@ export enum GameStates {
   lose = 2,
   win = 3,
 }
+
+export enum scoreColors {
+  common = "text-slate-200",
+  uncommon = "text-green-500",
+  rare = "text-sky-500",
+  epic = "text-violet-500",
+  legend = "text-yellow-500",
+  myth = "text-pink-500",
+}
