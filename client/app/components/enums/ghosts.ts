110ef02295d6c459d75c7af9e5f2aea40bd630d0
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-21 12:24:27 -0300

    create ghost enums

---
+++ client/app/components/enums/ghosts.ts
@@ -0,0 +1,13 @@
+export enum GhostState {
+  walking = "walking",
+  frightened = "frightened",
+  recovering = "recovering",
+  idle = "idle",
+}
+
+export enum GhostType {
+  inky = "inky",
+  blinky = "blinky",
+  pinky = "pinky",
+  clyde = "clyde",
+}
