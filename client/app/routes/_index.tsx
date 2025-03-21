75aacf4f821dbaebb9ec5b5e38689f19d4f1a297
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-21 12:26:04 -0300

    Update _index.tsx

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,9 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GameStates } from "~/components/enums/game";
 import { Scenes } from "~/components/enums/scene";
 import SceneDisplay from "~/components/sceneDisplay";
 import Button from "~/components/ui/button";
 import { RootState } from "~/store";
+import { setGameState, setPreviousGameState } from "~/store/gameSlice";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,8 +16,16 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const scene = useSelector((state: RootState) => state.scene);
+  const scene = useSelector((state: RootState) => state.scene.scene);
   const { highScore } = useSelector((state: RootState) => state.game);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (scene === Scenes.mainMenu) {
+      dispatch(setGameState(GameStates.start));
+      dispatch(setPreviousGameState(GameStates.start));
+    }
+  }, [scene, dispatch]);
   return (
     <main className="flex flex-col items-center w-full p-4 bg-slate-950 h-dvh">
       <h3 className="mb-4 font-bold tracking-wider text-center text-md text-slate-300">
@@ -24,7 +35,7 @@ export default function Index() {
         scene={Scenes.mainMenu}
         soundData={{ folder: "ui", audio: "menu" }}
         style={`${
-          scene.scene === Scenes.mainMenu ? "hidden" : "flex"
+          scene === Scenes.mainMenu ? "hidden" : "flex"
         } text-slate-300 bg-slate-800 ring-2 ring-slate-500 px-2 py-1 rounded-md`}
         label="Main Menu"
       />
