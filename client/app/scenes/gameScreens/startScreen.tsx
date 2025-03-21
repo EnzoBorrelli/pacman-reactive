c45c7fa368f420b8eee2b40ae1d2ba118e62d1c5
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-21 12:24:17 -0300

    create game Screens

---
+++ client/app/scenes/gameScreens/startScreen.tsx
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { GameStates } from "~/components/enums/game";
+import CharactersDisplay from "~/components/ui/startScreen/charactersDisplay";
+import SoundPlayer from "~/components/utils/soundPlayer";
+import { setGameState } from "~/store/gameSlice";
+
+export default function StartScreen() {
+  const dispatch = useDispatch();
+
+  const Click = () => {
+    dispatch(setGameState(GameStates.playing));
+    SoundPlayer({ folder: "ui", audio: "select" });
+  };
+  return (
+    <div>
+      <CharactersDisplay />
+      <button
+        onMouseEnter={() => SoundPlayer({ folder: "ui", audio: "hover" })}
+        onClick={Click}
+        className="text-white mt-20 text-2xl hover:text-yellow-400"
+      >
+        HI, Start this game
+      </button>
+    </div>
+  );
+}
