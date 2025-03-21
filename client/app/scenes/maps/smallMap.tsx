6e1f4f66697fb2af1b565456372c23cdc6e35018
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-03-21 12:25:55 -0300

    switch game controlle with states controller

--- client/app/scenes/maps/smallMap.tsx
+++ client/app/scenes/maps/smallMap.tsx
@@ -1,4 +1,4 @@
-import GameController from "~/components/controllers/gameController";
+import StatesController from "~/components/controllers/statesController";
 import PacmanDebug from "~/components/utils/debug/pacmanDebug";
 
 export default function SmallMap() {
@@ -6,7 +6,7 @@ export default function SmallMap() {
     <main className="flex flex-col items-center w-full p-4 pt-6 h-dvh">
       <div className="flex flex-row">
         <PacmanDebug isDebug={false} />
-        <GameController />
+        <StatesController/>
       </div>
     </main>
   );
