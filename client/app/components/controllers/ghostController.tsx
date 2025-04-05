620cca5d7b0cf801e2b4192ca4f4eb89f8f517fd
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-04-05 12:07:55 -0300

    made the controller usable for each ghost

--- client/app/components/controllers/ghostController.tsx
+++ client/app/components/controllers/ghostController.tsx
@@ -1,58 +1,114 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "~/store";
-import { GhostType } from "../enums/ghosts";
+import { GhostState, GhostType } from "../enums/ghosts";
 import Ghost from "../sprites/ghost";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Scenes } from "../enums/scene";
-import {
-  setGhostPosition,
-  setGhostSize,
-} from "~/store/ghostSlices/blinkySlice";
 import { ghostInitialPos } from "../enums/maps";
+import { UnknownAction } from "redux";
+import { Direction } from "../enums/global";
 
-export default function GhostController() {
-  const blinky = useSelector((state: RootState) => state.blinky);
+type Position = { x: number; y: number };
+interface GhostControllerProps {
+  ghostActions: {
+    setGhostSize: (size: number) => UnknownAction;
+    setGhostPosition: (position: Position) => UnknownAction;
+    setGhostDirection: (direction: Direction) => UnknownAction;
+    setGhostState: (state: GhostState) => UnknownAction;
+  };
+  ghost: {
+    position: Position;
+    size: number;
+    direction: Direction;
+    state: GhostState;
+  };
+  type: GhostType;
+}
+
+export default function GhostController({
+  ghostActions: {
+    setGhostSize,
+    setGhostPosition,
+    setGhostDirection,
+    setGhostState,
+  },
+  ghost,
+  type,
+}: GhostControllerProps) {
   const { scene } = useSelector((state: RootState) => state.scene);
   const dispatch = useDispatch();
+  const [posOffset, setPosOffset] = useState({ x: 0, y: 0 });
+  const [initialPos, setInitialPos] = useState({ x: 0, y: 0 });
+  const [spawn, setSpawn] = useState({ x: 0, y: 0 });
+
+  function addPositions(p1: Position, p2: Position): Position {
+    return {
+      x: p1.x + p2.x,
+      y: p1.y + p2.y,
+    };
+  }
 
   useEffect(() => {
     switch (scene) {
       case Scenes.challengeMap:
         dispatch(setGhostSize(12));
-        dispatch(setGhostPosition({ x: 24, y: 24 }));
+        setInitialPos(ghostInitialPos.challengeMap);
         // setSpeed(Math.round(4 * boost));
         break;
       case Scenes.classicMap:
         dispatch(setGhostSize(24));
-        dispatch(setGhostPosition({ x: 24, y: 24 }));
+        setInitialPos(ghostInitialPos.classicMap);
         //setSpeed(Math.round(8 * boost));
         break;
       case Scenes.smallMap:
         dispatch(setGhostSize(36));
-        dispatch(setGhostPosition(ghostInitialPos.smallMap));
+        setInitialPos(ghostInitialPos.smallMap);
         //setSpeed(Math.round(12 * boost));
         break;
       default:
         dispatch(setGhostSize(36));
-        dispatch(setGhostPosition(ghostInitialPos.smallMap));
+        setInitialPos(ghostInitialPos.smallMap);
         //setSpeed(Math.round(12 * boost));
         break;
     }
   }, [scene, dispatch]);
+
+  useEffect(() => {
+    switch (type) {
+      case GhostType.pinky:
+        setPosOffset({ x: 0, y: 0 });
+        break;
+      case GhostType.inky:
+        setPosOffset({ x: -ghost.size, y: 0 });
+        break;
+      case GhostType.clyde:
+        setPosOffset({ x: ghost.size, y: 0 });
+        break;
+      case GhostType.blinky:
+        setPosOffset({ x: 0, y: -ghost.size });
+        break;
+      default:
+        setPosOffset({ x: 0, y: 0 });
+        break;
+    }
+    setSpawn(addPositions(initialPos, posOffset));
+    dispatch(setGhostPosition(spawn));
+  }, [type, dispatch, spawn]);
+
   return (
     <span
       style={{
         position: "absolute",
-        top: `${blinky.position.y}px`,
-        left: `${blinky.position.x}px`,
+        top: `${ghost.position.y}px`,
+        left: `${ghost.position.x}px`,
         zIndex: 3,
       }}
     >
       <Ghost
-        size={blinky.size}
-        type={GhostType.blinky}
-        direction={blinky.direction}
-        state={blinky.state}
+        size={ghost.size}
+        type={type}
+        direction={ghost.direction}
+        state={ghost.state}
       />
     </span>
   );
