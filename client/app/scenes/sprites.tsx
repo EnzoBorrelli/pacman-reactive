f3cbd69e39fc7f79c6fd1b357bf630e26abc0f35
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-04-08 09:57:00 -0300

    update ghost component

--- client/app/scenes/sprites.tsx
+++ client/app/scenes/sprites.tsx
@@ -5,6 +5,7 @@ import Fruit from "../components/sprites/fruit";
 import MazePart from "../components/sprites/mazePart";
 import { CharacterSize, Direction } from "~/components/enums/global";
 import { PacState } from "~/components/enums/pacman";
+import { GhostState, GhostType } from "~/components/enums/ghosts";
 
 export default function Sprites() {
   return (
@@ -13,24 +14,48 @@ export default function Sprites() {
         Sprites Display
       </h2>
       <div className="flex items-center justify-center gap-6">
-        <Pacman size={CharacterSize.small} direction={Direction.right} animation={PacState.chop} />
-        <Dot size={16}  isPower={false}/>
-        <Dot size={16}  isPower={true} />
-        <Ghost map="classic" ghost="inky" direction="up" state="walking" />
-        <Ghost map="classic" ghost="blinky" direction="down" state="walking" />
-        <Ghost map="classic" ghost="pinky" direction="right" state="walking" />
-        <Ghost map="classic" ghost="clyde" direction="left" state="walking" />
+        <Pacman
+          size={CharacterSize.small}
+          direction={Direction.right}
+          animation={PacState.chop}
+        />
+        <Dot size={16} isPower={false} />
+        <Dot size={16} isPower={true} />
         <Ghost
-          map="classic"
-          ghost="pinky"
-          direction="right"
-          state="frightened"
+          size={CharacterSize.small}
+          type={GhostType.inky}
+          direction={Direction.up}
+          state={GhostState.walking}
         />
         <Ghost
-          map="classic"
-          ghost="clyde"
-          direction="left"
-          state="recovering"
+          size={CharacterSize.small}
+          type={GhostType.blinky}
+          direction={Direction.down}
+          state={GhostState.walking}
+        />
+        <Ghost
+          size={CharacterSize.small}
+          type={GhostType.pinky}
+          direction={Direction.left}
+          state={GhostState.walking}
+        />
+        <Ghost
+          size={CharacterSize.small}
+          type={GhostType.clyde}
+          direction={Direction.right}
+          state={GhostState.walking}
+        />
+        <Ghost
+          size={CharacterSize.small}
+          type={GhostType.clyde}
+          direction={Direction.right}
+          state={GhostState.frightened}
+        />
+        <Ghost
+          size={CharacterSize.small}
+          type={GhostType.clyde}
+          direction={Direction.right}
+          state={GhostState.recovering}
         />
       </div>
       <div className="flex items-center justify-center gap-6 mt-10">
