d84075b431547dfd260e7a495adce880167ea768
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-04-10 15:35:47 -0300

    added to next level logic

--- client/app/scenes/gameScreens/winScreen.tsx
+++ client/app/scenes/gameScreens/winScreen.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { GameStates } from "~/components/enums/game";
+import { RootState } from "~/store";
+import {
+  setGameLevel,
+  setGameState,
+  setPreviousGameState,
+} from "~/store/gameSlice";
 
 export default function WinScreen() {
+  const game = useSelector((state: RootState) => state.game);
+  const dispatch = useDispatch();
+
+  const handleNextLevel = () => {
+    dispatch(setPreviousGameState(GameStates.nextLevel));
+    dispatch(setGameState(GameStates.playing));
+    dispatch(setGameLevel(game.level + 1));
+  };
+
   return (
-    <div className='text-white'>WinScreen</div>
-  )
+    <div className="text-white">
+      <h1>YOU WON!</h1>
+      <h2>Level {game.level}</h2>
+      <h2>Score: {game.score}</h2>
+      <button onClick={handleNextLevel}>next level</button>
+    </div>
+  );
 }
